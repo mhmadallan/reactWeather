873def83b5dceb108c0d109ab9cffdb2b61a9166
mhmadallan <blawa3i@gmail.com>
2019-11-13 23:48:10 +0100

    Updates

---
+++ precache-manifest.6923d6fdd00ab669eb079331f71ca587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862faa35c73dba5d596d87ae0db9bdad",
+    "url": "/reactWeather/index.html"
+  },
+  {
+    "revision": "0131962bb54d624a37ca",
+    "url": "/reactWeather/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "62915516493b6d34be13",
+    "url": "/reactWeather/static/js/2.0f389746.chunk.js"
+  },
+  {
+    "revision": "0131962bb54d624a37ca",
+    "url": "/reactWeather/static/js/main.51aabdff.chunk.js"
+  },
+  {
+    "revision": "8ecefb3306c83187e888",
+    "url": "/reactWeather/static/js/runtime-main.fcddf526.js"
+  }
+]);
